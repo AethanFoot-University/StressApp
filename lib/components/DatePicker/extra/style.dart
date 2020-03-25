fe59f84ba4ebe1d052334407c52b9beb8786df1c
Mathew Allington <funkymallington@gmail.com>
2020-03-25 12:38:21 +0000

    Added DatePicker for custom modification from https://github.com/Mallington/DatePickerTimelineFlutter

---
+++ lib/components/DatePicker/extra/style.dart
@@ -0,0 +1,26 @@
+
+import 'package:flutter/material.dart';
+import 'package:stress_app/components/DatePicker/extra/color.dart';
+import 'package:stress_app/components/DatePicker/extra/dimen.dart';
+
+
+const TextStyle defaultMonthTextStyle = TextStyle(
+  color: AppColors.defaultMonthColor,
+  fontSize: Dimen.monthTextSize,
+  fontFamily: 'Roboto',
+  fontWeight: FontWeight.w500,
+);
+
+const TextStyle defaultDateTextStyle = TextStyle(
+  color: AppColors.defaultDateColor,
+  fontSize: Dimen.dateTextSize,
+  fontFamily: 'Roboto',
+  fontWeight: FontWeight.w500,
+);
+
+const TextStyle defaultDayTextStyle = TextStyle(
+  color: AppColors.defaultDayColor,
+  fontSize: Dimen.dayTextSize,
+  fontFamily: 'Roboto',
+  fontWeight: FontWeight.w500,
+);
--- lib/screens/graph_view/date_picker.dart
+++ lib/screens/graph_view/date_picker.dart
@@ -1,7 +1,8 @@
-import 'package:date_picker_timeline/date_picker_timeline.dart';
+
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:stress_app/components/DatePicker/date_picker_timeline.dart';
 import 'dart:core';
 import 'package:stress_app/data/StressLevel.dart';
 import 'package:stress_app/style/theme_colours.dart';
@@ -13,19 +14,28 @@ class DatePicker extends StatelessWidget {
 
 
   Widget drawMainScroll(){
+    var date =  DateTime.now().add(Duration(days: -10));
+    print(date.toIso8601String());
+    date.add(Duration(days: -10));
+    print(date.toIso8601String());
+
    return Column(
      mainAxisAlignment: MainAxisAlignment.center,
      children: <Widget>[
        DatePickerTimeline(
-         DateTime.now(),
+      DateTime.now(),
          onDateChange: (date) {
            // New date selected
            print(date.day.toString());
          },
+         startDate: DateTime.now().add(Duration(days: -20)),
          selectionColor: ThemeColours.SECONDARY_BACKGROUND_COLOR,
          dayTextStyle: TextStyle(color: Colors.white),
          dateTextStyle: TextStyle(color: Colors.white),
          monthTextStyle: TextStyle(color: Colors.white),
+         daysCount: 10,
+         
+
        ),
      ],
    );
